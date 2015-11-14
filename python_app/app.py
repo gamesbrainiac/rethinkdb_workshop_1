c439ab652f2986ee89903b354814022198f4af00
Chris Cates <codeviolet@chriscates.ca>
2015-11-14 18:35:49 -0500

    fiddling with python

--- python_app/app.py
+++ python_app/app.py
@@ -3,14 +3,28 @@ import tornado.ioloop
 import tornado.web
 
 r.set_loop_type("tornado")
+conn = yield r.connect(host="localhost", port=28015)
+sub = set()
 
-@gen.coroutine
-def messages
-    conn = yield r.connect(host="localhost", port=28015)
+
+class WSocketHandler(tornado.websocket.WebSocketHandler): #Tornado Websocket Handler
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        print "Client connected"
+        self.stream.set_nodelay(True)
+        sub.add(self)
+
+    def on_close(self):
+        print "Client disconnected"
+
+@tornado.gen.coroutine
+def getMessages()
     feed = yield r.db('workshop_1').table('messages').changes().run(conn)
     while (yield feed.fetch_next())
         change = yield feed.next()
-        print (change)
+        sub.write_message(change)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -19,10 +33,11 @@ class MainHandler(tornado.web.RequestHandler):
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/", MainHandler),
-        (r"/)
+        (r'/ws', WSocketHandler),
         (r'/(.*)', tornado.web.StaticFileHandler, {'path': './public/'}),
     ])
 
 print "Server running on Port 3002"
 application.listen(3002)
 tornado.ioloop.IOLoop.instance().start()
+tornado.ioloop.IOLoop.current().add_callback(getMessages)
